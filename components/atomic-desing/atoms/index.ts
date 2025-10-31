32422712e29a2e2f685b49def1b56d8d46001a9e
jwchavarrro <jwchavarro@prevalentware.com>
2025-10-31 16:15:21 -0500

    refactor(atoms/index.ts): update import paths for button, typography, and input components to use absolute paths for better organization and maintainability

--- components/atomic-desing/atoms/index.ts
+++ components/atomic-desing/atoms/index.ts
@@ -4,6 +4,6 @@
  */
 
 // Exportacion de componentes
-export * from "./button/index";
-export * from "./typography/index";
-export * from "./input/index";
+export * from "@/components/atomic-desing/atoms/button";
+export * from "@/components/atomic-desing/atoms/typography";
+export * from "@/components/atomic-desing/atoms/input";
