c79fd62a83913db2f82e57dfba17e5bfeca15172
jwchavarrro <jwchavarro@prevalentware.com>
2025-11-01 11:39:10 -0500

    refactor(atoms): replace old Button, Input, and Typography components with new versions and update export paths for consistency

--- components/atomic-desing/molecules/index.ts
+++ components/atomic-desing/molecules/index.ts
@@ -4,15 +4,15 @@
  */
 
 // Exportación de componentes
-export { Card } from "./card/index";
-export { BaseDialog } from "./dialog/base-dialog";
-export { DialogActions } from "./dialog/dialog-actions";
-export { DialogFooter } from "./dialog/dialog-footer";
-export { DialogHeader } from "./dialog/dialog-header";
+export { Card } from "./card1/index";
+export { BaseDialog } from "./dialog1/base-dialog";
+export { DialogActions } from "./dialog1/dialog-actions";
+export { DialogFooter } from "./dialog1/dialog-footer";
+export { DialogHeader } from "./dialog1/dialog-header";
 
 // Exportación de tipos
-export type { CardProps } from "./card/index";
-export type { BaseDialogProps } from "./dialog/base-dialog";
-export type { DialogActionsProps } from "./dialog/dialog-actions";
-export type { DialogFooterProps } from "./dialog/dialog-footer";
-export type { DialogHeaderProps } from "./dialog/dialog-header";
+export type { CardProps } from "./card1/index";
+export type { BaseDialogProps } from "./dialog1/base-dialog";
+export type { DialogActionsProps } from "./dialog1/dialog-actions";
+export type { DialogFooterProps } from "./dialog1/dialog-footer";
+export type { DialogHeaderProps } from "./dialog1/dialog-header";
