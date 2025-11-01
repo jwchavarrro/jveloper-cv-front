0d903a52ac9abbd71aa161750ddc1b36dce71bff
John Chavarro Urrea <147061325+jwchavarrro@users.noreply.github.com>
2025-11-01 10:56:34 -0500

    fix: add Button atom and ensure named exports

---
+++ components/atomic-desing/atoms/index.ts
@@ -0,0 +1,14 @@
+/**
+ * @file Atoms
+ * @description Componentes básicos e indivisibles
+ */
+
+// Exportacion de componentes
+export { Button } from "./button";
+export { Input } from "./input";
+export { Typography } from "./typography";
+
+// Exportacion de tipos
+export type { ButtonProps } from "./button";
+export type { TypographyProps } from "./typography";
+export type { InputProps } from "./input";
