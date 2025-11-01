5eb999fdcd58548b0e2df4cd93302d94520b7900
jwchavarrro <jwchavarro@prevalentware.com>
2025-11-01 14:20:26 -0500

    refactor(components): enhance Program component with customizable header and footer, update global styles for improved theming

--- components/pages/windows/programs/cv/cv.tsx
+++ components/pages/windows/programs/cv/cv.tsx
@@ -1,14 +1,18 @@
 import React from "react";
 
+// Import of components custom
 import { Program } from "@/components/pages/components/program";
+
+// Import of fragments
+import { HeaderCustom } from "./fragments/header-custom";
 
 export const CVProgram = () => {
   return (
     <Program
       open={true}
       onOpenChange={() => {}}
-      header={<div>Título de la ventana</div>}
-      footer={<div>Pie de página</div>}
+      headerCustom={<HeaderCustom />}
+      footerCustom={<div>Pie de página</div>}
     >
       Contenido
     </Program>
---
+++ components/pages/windows/programs/cv/fragments/index.ts
@@ -0,0 +1,8 @@
+/**
+ * @file fragments/index.ts
+ * @description Fragmentos de la ventana CV
+ * @module components/pages/windows/programs/cv/fragments/index.ts
+ */
+
+// Import of utilities
+export { HeaderCustom } from "./header-custom";
